7ca85b12d7ad2e5ccefce3843802afcdc880f1e4
eeellliiizzzkkk@yandex.ru <eeellliiizzzkkk@yandex.ru>
2022-05-24 21:46:24 +0300

    Коммит 2

--- test1.py
+++ test1.py
@@ -1,3 +1,8 @@
 print("Hello World!")
 print("My name is Liza")
 print("Whats your name?")
+name_user=input()
+print("Nice to meet you, " + name_user)
+print("Do you like this place?")
+question=input()
+print("Ohh, me too...")
