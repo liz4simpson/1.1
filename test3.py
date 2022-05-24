d01854bebdcd706029ee5a034ced9c480a14cd5c
eeellliiizzzkkk@yandex.ru <eeellliiizzzkkk@yandex.ru>
2022-05-24 22:08:47 +0300

    Коммит 5

---
+++ test3.py
@@ -0,0 +1,14 @@
+import math
+ 
+AB = input("Длина первого катета: ")
+AC = input("Длина второго катета: ")
+ 
+AB = float(AB)
+AC = float(AC)
+ 
+BC = math.sqrt(AB**2 + AC**2)
+ 
+P = AB + AC + BC
+ 
+print("Периметр треугольника: %.2f" % P)
+
